9cbefb08c13faa058a2b5afe64225019284f8da7
abc54396 <abc54396@NP2013115.addo.local>
2015-01-16 17:10:46 +0900

    bb

---
+++ src/first_yii/protected/data/schema.mysql_new.sql
@@ -0,0 +1,29 @@
+CREATE TABLE tbl_user (
+    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(128) NOT NULL,
+    password VARCHAR(128) NOT NULL,
+    email VARCHAR(128) NOT NULL
+);
+
+insert into testdrive.tbl_user(id,username,password,email) values (1,'test1','$2y$13$GdrtpDQHouElIvCJwenY.OOeooltq6OchymML7SPyDOOAJCfqvi3y','test1@example.com');
+insert into testdrive.tbl_user(id,username,password,email) values (2,'test2','pass2','test2@example.com');
+insert into testdrive.tbl_user(id,username,password,email) values (3,'test3','pass3','test3@example.com');
+insert into testdrive.tbl_user(id,username,password,email) values (4,'test4','pass4','test4@example.com');
+insert into testdrive.tbl_user(id,username,password,email) values (5,'test5','pass5','test5@example.com');
+insert into testdrive.tbl_user(id,username,password,email) values (6,'test6','pass6','test6@example.com');
+insert into testdrive.tbl_user(id,username,password,email) values (7,'test7','pass7','test7@example.com');
+insert into testdrive.tbl_user(id,username,password,email) values (8,'test8','pass8','test8@example.com');
+insert into testdrive.tbl_user(id,username,password,email) values (9,'test9','pass9','test9@example.com');
+insert into testdrive.tbl_user(id,username,password,email) values (10,'test10','pass10','test10@example.com');
+insert into testdrive.tbl_user(id,username,password,email) values (11,'test11','pass11','test11@example.com');
+insert into testdrive.tbl_user(id,username,password,email) values (12,'test12','pass12','test12@example.com');
+insert into testdrive.tbl_user(id,username,password,email) values (13,'test13','pass13','test13@example.com');
+insert into testdrive.tbl_user(id,username,password,email) values (14,'test14','pass14','test14@example.com');
+insert into testdrive.tbl_user(id,username,password,email) values (15,'test15','pass15','test15@example.com');
+insert into testdrive.tbl_user(id,username,password,email) values (16,'test16','pass16','test16@example.com');
+insert into testdrive.tbl_user(id,username,password,email) values (17,'test17','pass17','test17@example.com');
+insert into testdrive.tbl_user(id,username,password,email) values (18,'test18','pass18','test18@example.com');
+insert into testdrive.tbl_user(id,username,password,email) values (19,'test19','pass19','test19@example.com');
+insert into testdrive.tbl_user(id,username,password,email) values (20,'test20','pass20','test20@example.com');
+insert into testdrive.tbl_user(id,username,password,email) values (21,'test21','pass21','test21@example.com');
+insert into testdrive.tbl_user(id,username,password,email) values (22,'sugiyama','$2y$13$BwAPW9k7UK3uPetcT6aR8OLhQN3FZ/Z0FWoqrAbVDyt..Sq87lcTy','sugiyama@test.co.jp');
